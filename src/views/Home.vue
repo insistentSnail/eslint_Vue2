5b98b8c262fb703cd0580b1a485f81440f8329f2
Liu xin <1213736251@qq.com>
2021-08-17 14:52:34 +0800

    first

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2 class="timer">{{ time }}</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      time: 1,
+    }
+  },
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    add() {
+      if (this.time == 1) {
+        this.time = 2
+      }
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.home {
+  .timer {
+    background: #f40;
+  }
+}
+</style>
